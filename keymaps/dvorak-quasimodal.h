6fd77b24bec927a6854fac7977d481d09a681397
Jason Koh <WildlyManifest@fastmail.net>
2020-09-22 22:05:16 +1000

    Fix keymap

--- keymaps/dvorak-quasimodal.h
+++ keymaps/dvorak-quasimodal.h
@@ -33,41 +33,41 @@ KEYMAPS(
   /* Dvorak */
   [DVRK] = KEYMAP_STACKED
   (
-       Key_Quote       ,Key_Comma   ,Key_Period  ,Key_P         ,Key_Y
-      ,Key_A           ,Key_O       ,Key_E       ,Key_U         ,Key_I
-      ,Key_Semicolon   ,Key_Q       ,Key_J       ,Key_K         ,Key_X
-      ,Key_LeftControl ,Key_LeftGui ,Key_LeftAlt ,MO(FNNM)      ,Key_LeftShift ,Key_Backspace
+    Key_Quote ,Key_Comma ,Key_Period ,Key_P ,Key_Y
+    ,Key_A ,Key_O ,Key_E ,Key_U ,Key_I
+    ,Key_Semicolon ,Key_Q ,Key_J ,Key_K ,Key_X ,Key_Tab
+    ,Key_LeftControl ,Key_LeftGui ,Key_LeftAlt ,MO(FNNM) ,Key_LeftShift ,Key_Backspace
 
-                 ,Key_F          ,Key_G     ,Key_C        ,Key_R        ,Key_L
-                 ,Key_D          ,Key_H     ,Key_T        ,Key_N        ,Key_S
-                 ,Key_B          ,Key_M     ,Key_W        ,Key_V        ,Key_Z
-      ,Key_Space ,Key_RightShift ,MO(PUNV)  ,Key_RightAlt ,Key_RightGui ,Key_RightControl
+    ,Key_F ,Key_G ,Key_C ,Key_R ,Key_L
+    ,Key_D ,Key_H ,Key_T ,Key_N ,Key_S
+    ,Key_Enter ,Key_B ,Key_M ,Key_W ,Key_V ,Key_Z
+    ,Key_Space ,Key_RightShift ,MO(PUNV) ,Key_RightAlt ,Key_RightGui ,Key_RightControl
   ),
   /* Function keys, numeric keypad */
   [FNNM] = KEYMAP_STACKED
   (
-       Key_F11 ,Key_F12 ,System_PowerDown ,M(RESET) ,Key_Sysreq
-      ,Key_F6  ,Key_F7  ,Key_F8           ,Key_F9   ,Key_F10
-      ,Key_F1  ,Key_F2  ,Key_F3           ,Key_F4   ,Key_F5
-      ,___     ,___     ,___              ,___      ,___     ,Key_Escape
+    Key_F11 ,Key_F12 ,Key_Power ,XXX ,Key_Sysreq
+    ,Key_F6 ,Key_F7 ,Key_F8 ,Key_F9 ,Key_F10
+    ,Key_F1 ,Key_F2 ,Key_F3 ,Key_F4 ,Key_F5 ,___
+    ,___ ,___ ,___ ,___ ,___ ,___
 
-                  ,Key_KeypadDivide   ,Key_Keypad7  ,Key_Keypad8  ,Key_Keypad9        ,Key_KeypadSubtract
-                  ,Key_KeypadMultiply ,Key_Keypad4  ,Key_Keypad5  ,Key_Keypad6        ,Key_KeypadAdd
-                  ,Key_KeypadEquals   ,Key_Keypad1  ,Key_Keypad2  ,Key_Keypad3        ,Key_KeypadEnter
-      ,Key_Enter  ,___                ,Key_Keypad0  ,___          ,RGUI_T(KeypadDot)  ,___
-   ),
+    ,Key_KeypadDivide ,Key_Keypad7 ,Key_Keypad8 ,Key_Keypad9 ,Key_KeypadSubtract
+    ,Key_KeypadMultiply ,Key_Keypad4 ,Key_Keypad5 ,Key_Keypad6 ,Key_KeypadAdd
+    ,Key_KeypadEnter ,Key_KeypadEquals ,Key_Keypad1 ,Key_Keypad2 ,Key_Keypad3 ,Key_KeypadDot
+    ,___ ,___ ,Key_Keypad0 ,___ ,___ ,___
+  ),
   /* Punctuation, Navigation */
   [PUNV] = KEYMAP_STACKED
   (
-       Key_Backtick ,Key_Slash      ,Key_Backslash  ,Key_LeftBracket  ,Key_RightBracket
-      ,Key_6        ,Key_7          ,Key_8          ,Key_9            ,Key_0
-      ,Key_1        ,Key_2          ,Key_3          ,Key_4            ,Key_5
-      ,___          ,LGUI_T(Minus)  ,LALT_T(Equals) ,___              ,___              ,Key_Tab
+    Key_Backtick ,Key_Slash ,Key_Backslash ,Key_LeftBracket ,Key_RightBracket
+    ,Key_6 ,Key_7 ,Key_8 ,Key_9 ,Key_0
+    ,Key_1 ,Key_2 ,Key_3 ,Key_4 ,Key_5 ,Key_Equals 
+    ,___ ,___ ,___ ,Key_Escape ,___ ,Key_Minus
 
-                        ,Key_PageUp               ,Key_Home                   ,Key_UpArrow              ,Key_End                ,Key_Insert
-                        ,Key_PageDown             ,Key_LeftArrow              ,Key_DownArrow            ,Key_RightArrow         ,Key_Delete
-                        ,Consumer_VolumeDecrement ,Consumer_ScanPreviousTrack ,Consumer_PlaySlashPause  ,Consumer_ScanNextTrack ,Consumer_VolumeIncrement
-      ,LSHIFT(Key_Tab)  ,___                      ,___                        ,___                      ,___                    ,___
-   )
+    ,Key_PageUp ,Key_Home ,Key_UpArrow ,Key_End ,Key_Insert
+    ,Key_PageDown ,Key_LeftArrow ,Key_DownArrow  ,Key_RightArrow ,Key_Delete
+    ,Consumer_VolumeIncrement ,XXX ,Consumer_ScanPreviousTrack ,Consumer_PlaySlashPause ,Consumer_ScanNextTrack ,XXX
+    ,Consumer_VolumeDecrement ,___ ,___ ,___ ,___ ,___
+  )
 )
 /* *INDENT-ON* */
